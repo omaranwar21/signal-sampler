011841b73740da156d4c89a9a08d94f64a08ba8b
youssef Shaban <85808789+youssef-shaban@users.noreply.github.com>
2022-10-15 16:19:47 +0200

    sum of signal, adding sampling and noise

--- app.py
+++ app.py
@@ -1,9 +1,7 @@
-from audioop import add
-from os import remove
-from signal import signal
 import streamlit as st
-import matplotlib.pyplot as plt
-from utils import samplingRate
+from utils import samplingRate, signal_sum, sampled_signal, add_noise
+import numpy as np
+import plotly.graph_objects as go
 st.set_option('deprecation.showPyplotGlobalUse', False)
 st.set_page_config(
     page_title="Signal Sampler",
@@ -17,10 +15,7 @@ def add_simulated_signal():
         "freq_value":st.session_state.freq_value,
         "freq_scale":st.session_state.add_signal_freq_scale,
         "mag_value":st.session_state.mag_value
-    }
-def remove_simulated_signal():
-    pass
-    
+    }    
 
 if "simulated_signal" not in st.session_state:
     st.session_state.simulated_signal= {}
@@ -42,11 +37,11 @@ with c1:
 
 ce, c1, ce, c2, ce, c4,ce = st.columns([0.07, 1, 0.07, 3.5, 0.07,1,0.07])
 with c1:
-    sampling_rate_scale= st.selectbox("Scale of freq.",("10Hz","100Hz","1KHz","10KHz","100KHz","F(max)Hz"))
-    maxV, step, format= samplingRate(sampling_rate_scale)
+    sampling_rate_scale= st.selectbox("Scale of freq.",("10Hz","100Hz","1KHz","10KHz","100KHz","F(max)Hz"),key="sampling_rate_scale")
+    maxV, minV,step, format= samplingRate(sampling_rate_scale)
     sampling_rate = st.slider(
             "sampling rate",
-            min_value=0.0,
+            min_value=minV,
             max_value=maxV,
             step=step,
             format=format,
@@ -54,7 +49,7 @@ with c1:
         )
     noise_checkbox=st.checkbox("Add Noise",key="noise_checkbox")
     if noise_checkbox:
-        noise=st.slider("SNR",key="noise_slider")
+        noise=st.slider("SNR",min_value=1.0,step=0.5,max_value=100.0,key="noise_slider")
 
     with st.expander("More Options"):
         st.write("")
@@ -67,17 +62,16 @@ with c4:
         if add_signal:
             with st.form("add_signal_form"):
                 signal_name= st.text_input("Enter Signal Name", key="signal_name")
-                sampling_rate_scale= st.selectbox("Scale of freq.",("10Hz","100Hz","1KHz","10KHz","100KHz"),key="add_signal_freq_scale")
-                maxV, step, format= samplingRate(sampling_rate_scale)
+                sampling_rate_scale= st.selectbox("Scale of freq.",("100Hz","100KHz"),key="add_signal_freq_scale")
                 signal_freq = st.slider(
                         "Choose Signal freqency",
                         min_value=0.0,
-                        max_value=maxV,
-                        step=step,
-                        format=format,
+                        max_value=100.0,
+                        step=0.5,
+                        value=1.0,
                         key="freq_value"
                     )
-                signal_mag= st.slider("Choose Signal magnitude",key="mag_value")
+                signal_mag= st.slider("Choose Signal magnitude",value=1.0,min_value=0.0,max_value=100.0,step=0.5,key="mag_value")
                 add_button=st.form_submit_button("Add Signal",on_click=add_simulated_signal)
 
 
@@ -86,12 +80,48 @@ with c4:
             remove_button=st.button("remove")
             if remove_button:
                 del st.session_state.simulated_signal[remove_box]
-    selected_graphs= st.selectbox("Select type of graph",("Signal with Samples","Samples Only","Signal Only","Reconstructed Signal"))        
+    selected_graphs= st.selectbox("Select type of graph",("Signal with Samples","Samples Only","Signal Only","Reconstructed Signal"),key="graph_type")        
         
                 
 
 
 
 with c2:
-    
-    st.line_chart()
+    signal_flag=False
+    sample_flag=False
+    reconstruction_flag=False
+    if(st.session_state.graph_type=="Signal with Samples"):
+        signal_flag=True
+        sample_flag=True
+        reconstruction_flag=False
+    elif(st.session_state.graph_type=="Samples Only"):
+        sample_flag=True
+        signal_flag=False
+        reconstruction_flag=False
+    elif(st.session_state.graph_type=="Signal Only"):
+        signal_flag=True
+        sample_flag=False
+        reconstruction_flag=False
+    elif(st.session_state.graph_type=="Reconstructed Signal"):
+        signal_flag=False
+        sample_flag=True
+        reconstruction_flag=True
+
+    time= np.linspace(0, 3, 1500)
+    full_signals=signal_sum(st.session_state.simulated_signal,time)
+
+    if st.session_state.noise_checkbox:
+        full_signals=add_noise(full_signals,st.session_state.noise_slider)
+    fig = go.Figure()
+    if signal_flag:
+        fig.add_trace(go.Scatter(x=time, y=full_signals,
+                    mode='lines',
+                    name='lines'))
+    if sample_flag:
+        sampled_x, sampled_time=sampled_signal(full_signals,time, st.session_state.sampling_rate, st.session_state.sampling_rate_scale)
+        fig.add_trace(go.Scatter(x=sampled_time, y=sampled_x,
+                    mode='markers',
+                    name='markers'))
+    fig.update_xaxes(showgrid=False)
+    fig.update_yaxes(showgrid=False)
+    st.plotly_chart(fig,use_container_width=True)
--- utils.py
+++ utils.py
@@ -1,26 +1,63 @@
+import numpy as np
 def samplingRate(rate):
     if rate=="10Hz":
         maxV= 10.0
+        minV=1.0
         step=0.5
         format="%fHz"
     elif rate=="100Hz":
         maxV= 100.0
+        minV=1.0
         step=1.0
         format="%dHz"
     elif rate=="1KHz":
         maxV=1.0
+        minV=0.1
         step=0.1
         format="%fKHz"
     elif rate=="10KHz":
         maxV= 10.0
+        minV=1.0
         step=0.5
         format="%fKHz"
     elif rate== "100KHz":
         maxV= 100.0
+        minV=1.0
         step=1.0
         format="%dKHz"
     elif rate=="F(max)Hz":
         maxV= 100.0
+        minV=1.0
         step=1.0
         format="%dKHz"
-    return maxV, step, format
+    return maxV,minV, step, format
+
+def signal_sum(Signals,t):
+    keys= Signals.keys()
+    Sum=np.zeros(len(t))
+    for i in keys:
+        factor=1
+        if Signals[i]["freq_scale"]=="100KHz":
+            factor=1000
+        signal= Signals[i]["mag_value"]*np.sin(2*np.pi*Signals[i]["freq_value"]*t*factor)
+        Sum+=signal
+    return Sum
+
+def sampled_signal(signal, time,sample_freq,scale):
+    factor=1
+    if scale in ["1KHz","10KHz","100KHz"]:
+        factor=1000
+    sample_rate= int((len(time)/time[-1])/(sample_freq*factor))
+    if sample_rate==0:
+        sample_rate=1
+    sampled_time= time[::sample_rate]
+    sampled_signal= signal[::sample_rate]
+    return sampled_signal, sampled_time
+
+def add_noise(signal, SNR):
+    noise= np.random.randn(len(signal))
+    Es= np.sum(signal**2)
+    En= np.sum(noise**2)
+    alpha= np.sqrt(Es/(SNR*En))
+    d= signal+noise*alpha
+    return d
