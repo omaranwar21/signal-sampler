01fd73dc16eedaa71b4c9d48a1ddd548324cd33f
Omar-Saad-ELGharbawy <84602951+Omar-Saad-ELGharbawy@users.noreply.github.com>
2022-10-21 03:21:13 +0200

    adding some comments

--- app.py
+++ app.py
@@ -17,6 +17,7 @@ render_svg("svg.svg")
 with open("style.css") as design:
     st.markdown(f"<style>{design.read()}</style>", unsafe_allow_html=True)
 
+# Initialization of Session State attributes (time,uploaded_signal,maxf)
 if 'time' not in st.session_state:
     st.session_state.time =np.linspace(0,5,2000)
 if 'uploaded_signal' not in st.session_state:
@@ -34,6 +35,7 @@ def add_simulated_signal():
         "mag_value":st.session_state.mag_value
     }    
 
+# Initialization of Session State attribute (simulated_signal)
 if "simulated_signal" not in st.session_state:
     st.session_state.simulated_signal= {}
 
@@ -49,12 +51,16 @@ with c1:
         st.session_state.maxf= maxF
     
 ce, c1, ce, c2, c4, ce = st.columns([0.07, 1, 0.07, 3.5, 1, 0.07])
+#column 1 responsible for sampling rate slider and adding noise
 with c1:
     sampling_options=("10Hz","100Hz","1KHz","10KHz","100KHz")
     if st.session_state.uploaded_file:
         sampling_options=("10Hz","100Hz","1KHz","10KHz","100KHz","F(max)Hz")
+    #sampling_rate_scale variable to store scale of frequency from selectbox
     sampling_rate_scale= st.selectbox("Scale of freq.",sampling_options,key="sampling_rate_scale")
+    #getting maxV, minV,step,format values from samplingRate() function
     maxV, minV,step, format= samplingRate(sampling_rate_scale)
+    #adding sampling rate slider to get sampling rate from user
     sampling_rate = st.slider(
             "sampling rate",
             min_value=minV,
@@ -67,12 +73,17 @@ with c1:
     if noise_checkbox:
         noise=st.slider("SNR",min_value=1.0,step=0.5,max_value=100.0,key="noise_slider")
 
+#column 4 responsible for uploading and simulating signals
 with c4: 
+    #radio buttons to select to upload or simulate signal
     choose_signal= st.radio("Choose Signal",options=("Uploaded Signal","Simulating"),horizontal=True, key="choose_signal")
     if choose_signal=="Simulating":
+        #slider to select signal period
         signal_period= st.slider("Signal Period",min_value=0.1,max_value=10.0,step=0.1,value=1.0,format="%fsec",key="signal_period")
+        #button to add signal
         add_signal=st.button("Add Signal")
         if add_signal:
+            #form to take added signal values(Signal Name, Signal Frequency, Signal Magnitude) from user
             with st.form("add_signal_form"):
                 signal_name= st.text_input("Enter Signal Name", key="signal_name")
                 sampling_rate_scale= st.selectbox("Scale of freq.",("100Hz","100KHz"),key="add_signal_freq_scale")
